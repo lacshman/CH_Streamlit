2b38e75586c525adb95eb86cf28bf02a7a50f8f0
LakshmanKumar <31729621+lacshman@users.noreply.github.com>
2024-10-13 09:45:29 +0530

    Update streamlit_test_1.py

--- streamlit_test_1.py
+++ streamlit_test_1.py
@@ -1,3 +1,11 @@
 import streamlit as st
-st.write("Hello Nature")
-st.text_input("Favourite Book name")
+hide_menu ="""
+<style>
+#MainMenu { visibility: hidden;}
+</style>
+"""
+def main():
+  st.title("corro")
+  st.markdown(hide_menu, unsafe_allow_html=True)
+  st.write("Hello Nature")
+  st.text_input("Favourite Book name")
